3bc5e06e6f9145701417cbbca8bf246cc630178e
woutergoku <wouter1993@hotmail.com>
2018-11-08 16:13:53 +0100

    Added search function

--- SSO-app/src/app/app.module.ts
+++ SSO-app/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { HttpModule } from '@angular/http';
+import { SearchService } from './shared';
+
+const appRoutes: Routes = [
+  {path: 'search', component: SearchComponent},
+  {path: '', redirectTo: '/search', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [SearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ SSO-app/src/app/search/search.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Person, SearchService } from '../shared';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+  query: string;
+  searchResults: Array<Person>;
+
+  constructor(private searchService: SearchService) { }
+
+  search(): void {
+    this.searchService.search(this.query).subscribe(
+      data => { this.searchResults = data; },
+      error => console.log(error)
+    );
+  }
+
+  ngOnInit() {
+    this.search();
+  }
+
+}
